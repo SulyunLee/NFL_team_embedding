d200bbd84d3bff1108ec6a57ee1a8d08c72187fc
Sulyun Lee <sulyun-lee@uiowa.edu>
2021-03-26 08:16:26 -0500

    Add script

--- src/generate_coach_features.py
+++ src/generate_coach_features.py
@@ -4,6 +4,51 @@ This script generates feature vectors for each coach in each season.
 import pandas as pd
 import numpy as np
 from POSITION_ASSIGNMENT import *
+from tqdm import tqdm
+
+def total_years_NFL_feature(row, df):
+    name = row.Name
+    year = row.Year
+    
+    # extract the coach'es previous year seasons
+    previous_NFL_seasons = df[(df.Name == name) & (df.Year < year)]
+    # count the number of years
+    total_years_NFL = previous_NFL_seasons.Year.unique().shape[0]
+
+    return total_years_NFL
+
+def past_seasons_winning_features(row, record_df, win_result_df):
+    name = row.Name
+    year = row.Year
+
+    # collect last 5 years of game records
+    previous_5yr_NFL_seasons = record_df[(record_df.Name == name) & (record_df.Year < year) & (record_df.Year >= year-5)]
+    # retrieve the game results (winning percentages)
+    previous_5yr_NFL_seasons_results = previous_5yr_NFL_seasons.merge(win_result_df, how="left") 
+
+    # compute best and average winning percentages
+    best_prev5yr_winperc = previous_5yr_NFL_seasons_results.Win_Percentage.max()
+    avg_prev5yr_winperc = round(previous_5yr_NFL_seasons_results.Win_Percentage.mean(),3)
+
+    return best_prev5yr_winperc, avg_prev5yr_winperc
+
+def position_name_features(row):
+    position_id = row.final_position
+    hier_num = row.final_hier_num
+
+    # head coach indicator
+    if position_id == "HC": 
+        HC = 1
+    else:
+        HC = 0
+
+    # Coordinator indicator
+    if hier_num == 2:
+        Coord = 1
+    else:
+        Coord = 0
+
+    return HC, Coord
 
 if __name__ == "__main__":
     #################################################################
@@ -21,28 +66,40 @@ if __name__ == "__main__":
     total_win_df = pd.read_csv(total_win_filename)
     #################################################################
 
-    row = NFL_record_df.iloc[100]
-    name = row.Name
-    team = row.Team
-    year = row.Year
-    position_id = row.final_position
-    hier_num = row.final_hier_num
+    tqdm.pandas()
 
-    # Total years in NFL
-    previous_NFL_seasons = NFL_record_df[(NFL_record_df.Name == name) & (NFL_record_df.Year < year)]
-    total_years_NFL = previous_NFL_seasons.Year.unique().shape[0]
+    # total win dataset modification
+    print("Calculating winning percentages...")
+    win_perc = total_win_df.Total_Win / 16
+    team_name_modified = total_win_df.Team + " (NFL)"
+    total_win_df = total_win_df.assign(Win_Percentage = win_perc.round(3))
+    total_win_df = total_win_df.assign(Team = team_name_modified)
 
-    # Winning percentage during the past 5 years as college or NFL coach
+    ## Feature 1: Total years in NFL
+    print("Feature 1: Total years in NFL")
+    total_years_NFL = NFL_record_df.progress_apply(total_years_NFL_feature, \
+            args=[NFL_record_df], axis=1)
+    NFL_record_df = NFL_record_df.assign(TotalYearsInNFL = total_years_NFL)
 
-    # HC indicator
+    ## Feature 2: Winning percentage during the past 5 years as college or NFL coach
+    print("Feature 2: Best winning percentage during the past 5 years in NFL")
+    print("Feature 3: Average winning percentage during the past 5 years in NFL")
+    best_prev5yr_winperc, avg_prev5yr_winperc = zip(*NFL_record_df.progress_apply(past_seasons_winning_features, args=[NFL_record_df, total_win_df], axis=1))
+    NFL_record_df = NFL_record_df.assign(Past5yrsWinningPerc_best = best_prev5yr_winperc)
+    NFL_record_df = NFL_record_df.assign(Past5yrsWinningPerc_avg = avg_prev5yr_winperc)
+
+    ## Feature 3: Position names (head coach or coordinator)
+    print("Feature 4: Head coach")
+    print("Feature 5: Coordinator")
     # distinguish head coaches by position ID because there is interim HC.
-    if position_id == "HC": 
-        HC = 1
-    else:
-        HC = 0
+    HC, Coord = zip(*NFL_record_df.progress_apply(position_name_features, axis=1))
+    NFL_record_df = NFL_record_df.assign(HC=HC)
+    NFL_record_df = NFL_record_df.assign(Coord=Coord)
 
-    # Coordinator indicator
-    if position_hier == 2:
-        Coord = 1
-    else:
-        Coord = 0
+    ## Feature 4: Node embedding (collaboration)
+
+
+
+    NFL_record_df.to_csv("../datasets/2002-2019_NFL_Coach_Data_with_features.csv",\
+            index=False, encoding="utf-8-sig")
+    
